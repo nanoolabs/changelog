6e8b4223afb43be5d2e06f3772ad9f9ad2a61193
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-07-10 13:56:41 +0700

    fix(script): remove flag b2 hard delete

--- scripts/sync-b2.sh
+++ scripts/sync-b2.sh
@@ -5,9 +5,9 @@ SOURCE="brands/"
 BUCKET="${B2_BUCKET_NAME}"
 DRY_RUN="--dry-run"
 [ "$1" = "confirm" ] && DRY_RUN=""
-rclone sync "$SOURCE" :b2:"$BUCKET" \
+rclone sync "$SOURCE" :b2:"$BUCKET"/brands \
   $DRY_RUN \
   --b2-account "$B2_KEY_ID" \
   --b2-key "$B2_APPLICATION_KEY" \
-  --b2-hard-delete --progress --fast-list
+  --progress --fast-list
 echo "Sync complete"
